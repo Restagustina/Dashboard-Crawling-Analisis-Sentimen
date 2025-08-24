8f10840ef3153b3ef85310d7aa93dc3a93d01046
Resta Gustina <restaagustina4@gmail.com>
2025-08-24 16:04:52 +0700

    Remove supabase.py to fix circular import

--- supabase.py
+++
@@ -1,7 +0,0 @@
-import os
-from supabase import create_client as create_supabase_client
-
-def create_client():
-    url = os.environ.get("SUPABASE_URL")
-    key = os.environ.get("SUPABASE_KEY")
-    return create_supabase_client(url, key)
