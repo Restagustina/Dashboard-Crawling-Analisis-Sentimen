d4136a6b61ef02bfa0539bd2b8c8e79492297bdb
Resta Gustina <restaagustina4@gmail.com>
2025-08-26 06:13:39 +0700

    Perbaiki setting versi chromedriver di webdriver-manager

--- crawling.py
+++ crawling.py
@@ -38,8 +38,10 @@ def get_chrome_driver(headless=True):
 
     user_agent = os.getenv("USER_AGENT", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
     options.add_argument(f"user-agent={user_agent}")
-    driver_version = "139.0.7258.127"
-    driver_path = ChromeDriverManager(driver_version=driver_version).install()
+
+    driver_version = "139.0.7258.127"  # Sesuaikan dengan versi Chrome yang terinstall di runner
+    # Panggil install dengan versi chromedriver
+    driver_path = ChromeDriverManager().install(version=driver_version)
     driver = webdriver.Chrome(service=Service(driver_path), options=options)
     return driver
 
